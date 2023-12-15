5c54415dc97221023d6f4614d3dd32335eb028a1
MNeMoNiCuZ <60541708+MNeMoNiCuZ@users.noreply.github.com>
2023-12-15 01:14:05 +0100

    Updated the loading  message and removed comments

--- __init__.py
+++ __init__.py
@@ -1,9 +1,7 @@
 from .nodes.nodes import *
 
 NODE_CLASS_MAPPINGS = { 
-    #"Print Hello World": PrintHelloWorld,
     "Save Text File_mne": SaveTextFile,
-    #"Download Image_mne": FetchAndSaveImage,
     }
     
-print("\033[34mComfyUI Tutorial Nodes: \033[92mLoaded\033[0m")
+print("\033[34mMNeMiC Nodes: \033[92mLoaded\033[0m")
