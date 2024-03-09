3b23f3c1e7aa7d7c9d71ffc071b226e64fcad893
MNeMoNiCuZ <60541708+MNeMoNiCuZ@users.noreply.github.com>
2024-03-09 16:23:38 +0100

    Update __init__.py
    Added GenerateNegativePrompt node

--- __init__.py
+++ __init__.py
@@ -2,6 +2,7 @@ from .nodes.nodes import *
 
 NODE_CLASS_MAPPINGS = { 
     "Save Text File_mne": SaveTextFile,
+    "Generate Negative Prompt_mne": GenerateNegativePrompt,
     }
     
 print("\033[34mMNeMiC Nodes: \033[92mLoaded\033[0m")
