721c31973b3659f0f6767cc26db45ec60c4c2495
MNeMoNiCuZ <60541708+MNeMoNiCuZ@users.noreply.github.com>
2024-06-02 21:41:27 +0200

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -1,10 +1,13 @@
-from .nodes.nodes import *
+from .nodes.fetch_and_save_image import FetchAndSaveImage
+from .nodes.generate_negative_prompt import GenerateNegativePrompt
+from .nodes.groq_api_completion import GroqAPICompletion
+from .nodes.save_text_file import SaveTextFile
 
 NODE_CLASS_MAPPINGS = { 
     "💾 Save Text File With Path": SaveTextFile,
     "🖼️ Download Image from URL": FetchAndSaveImage,
     "✨ Groq LLM API": GroqAPICompletion,
     "⛔ Generate Negative Prompt": GenerateNegativePrompt,
-    }
-    
+}
+
 print("\033[34mMNeMiC Nodes: \033[92mLoaded\033[0m")
