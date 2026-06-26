0a04ef34e9fb8ed3acfc7d070afbaf7d208df521
MNeMoNiCuZ <mnemic@gmail.com>
2026-06-26 13:54:12 +0200

    Updated the supported groq models with current deprecation

--- nodes/groq_api_llm.py
+++ nodes/groq_api_llm.py
@@ -15,11 +15,10 @@ class GroqAPILLM:
     # Deprecation List - https://console.groq.com/docs/deprecations
 
     LLM_MODELS = [
-        "llama-3.1-8b-instant",
+        "openai/gpt-oss-20b",
+        "openai/gpt-oss-120b",
         "llama-3.3-70b-versatile",
         "moonshotai/kimi-k2-instruct-0905",
-        "openai/gpt-oss-120b",
-        "openai/gpt-oss-20b",
         "deepseek-r1-distill-llama-70b",
         "qwen-qwq-32b",
         "gemma2-9b-it",
